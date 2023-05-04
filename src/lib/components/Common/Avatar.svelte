5ffd0f5b375ffd679c123da6be3057ca767028ae
saklani <50768838+saklani@users.noreply.github.com>
2023-05-05 00:23:22 +0530

    feat: avatar added

---
+++ src/lib/components/Common/Avatar.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { account } from "sveeeth";
+
+    /** @param {string} address*/
+    function addressToGradient(address) {
+        const startColor = `#${address.substring(5, 11)}`;
+        const endColor = `#${address.substring(12, 18)}`;
+        return `linear-gradient(${startColor}, ${endColor})`;
+    }
+
+    /** @type {string}*/
+    let borderRadius = "999px";
+
+    /** @type {string}*/
+    let radius = "64px";
+
+    /** @type {string | undefined}*/
+    let src;
+
+    export { borderRadius, radius, src };
+</script>
+
+{#if src}
+    <img
+        style="height: {radius}; width: {radius}; border-radius: {borderRadius};"
+        alt="avatar"
+        {src}
+    />
+{:else}
+    <div
+        style="height: {radius}; width: {radius}; border-radius: 999px; background:{addressToGradient(
+            $account.address
+        )};"
+    />
+{/if}
--- src/lib/components/ConnectedButton.svelte
+++ src/lib/components/ConnectedButton.svelte
@@ -2,6 +2,7 @@
   import { account } from "sveeeth";
   import { shortAddress } from "../utils.js";
   import Address from "./Common/Address.svelte";
+  import Avatar from "./Common/Avatar.svelte";
   import Disconnect from "./Common/Disconnect.svelte";
   import ModalBox from "./Common/ModalBox.svelte";
 
@@ -10,11 +11,13 @@
   const handleClick = () => (showModal = true);
 </script>
 
-<button class="black" on:click={handleClick}>
+<button class="black row align gap padding-inline" on:click={handleClick}>
+  <Avatar radius={"26px"} />
   {shortAddress($account.address)}
 </button>
 
 <ModalBox title={"Connected"} bind:showModal>
+  <Avatar />
   <Address />
   <Disconnect />
 </ModalBox>
