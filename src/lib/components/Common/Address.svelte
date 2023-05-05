ea3e4efe5629f23d2a3b7e932f86a7aace74bfab
saklani <50768838+saklani@users.noreply.github.com>
2023-05-05 12:51:20 +0530

    fix: improve address

--- src/lib/components/Common/Address.svelte
+++ src/lib/components/Common/Address.svelte
@@ -1,12 +1,60 @@
 <script>
     import { shortAddress } from "../../utils.js";
-    import Copy from "./Copy.svelte";
+    import check from "$lib/images/check.svg";
+    import copy from "$lib/images/copy.svg";
+    import { sleep } from "$lib/utils.js";
+    import { fade } from "svelte/transition";
 
     /** @type {`0x{string}`}  */
-    export let address;
+    let address;
+
+    /** @type {string}  */
+    let fontSize = "1rem";
+
+    /** @type {`#{string}`} */
+    let color = "#000000";
+
+    /** @type {`#{string}`} */
+    let hoverColor = "#5F5F5F";
+
+    /** @type {`#{string}`} */
+    let _color = color;
+
+    let copied = false;
+    /** @param  {string} text */
+    const copyFn = async (text) => {
+        try {
+            copied = true;
+            await navigator.clipboard.writeText(text);
+            await sleep(3000);
+        } finally {
+            copied = false;
+        }
+    };
+
+    export { address, color, fontSize, hoverColor };
 </script>
 
-<div class="row align gap">
-    <h2>{shortAddress(address)}</h2>
-    <Copy text={address} />
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+    class="row align gap"
+    style="color: {_color}; cursor: pointer;"
+    title="copy"
+    aria-label="address"
+    on:click={() => copyFn(address)}
+    on:mouseenter={() => (_color = hoverColor)}
+    on:mouseleave={() => (_color = color)}
+>
+    <h3 style="font-size: {fontSize}">
+        {shortAddress(address)}
+    </h3>
+    {#if copied}
+        <div in:fade={{ delay: 500, duration: 500 }} class="row icon center">
+            <img style="height: 16px; width: 16px; color: {_color};" src={check} alt="copied" />
+        </div>
+    {:else}
+        <div in:fade={{ delay: 300 }} out:fade class="row icon center">
+            <img style="height: 16px; width: 16px; color: {_color};" src={copy} alt="copy" />
+        </div>
+    {/if}
 </div>
