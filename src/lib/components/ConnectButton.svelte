68021abd776a8b0fb23fd240f297f8750fa8d779
saklani <50768838+saklani@users.noreply.github.com>
2023-05-06 22:52:18 +0530

    fix: ConnectButton.svelte formatting + types"

--- src/lib/components/ConnectButton.svelte
+++ src/lib/components/ConnectButton.svelte
@@ -1,14 +1,14 @@
 <script>
     import sveeeth, { account, configureChains, network } from "sveeeth";
-    import { chainStore } from "../chains.js";
     import { publicProvider } from "sveeeth/providers";
+    import { chainStore } from "../chains.js";
     import Connected from "./Connected.svelte";
     import Disconnected from "./Disconnected.svelte";
     import Loading from "./Loading.svelte";
     import Unsupported from "./Unsupported.svelte";
     import "./style.css";
 
-    /** @type {import("@wagmi/core").Chain}*/
+    /** @type {import("@wagmi/core").Chain | undefined}*/
     let chains;
 
     const { provider } = configureChains(chains ?? $chainStore, [
