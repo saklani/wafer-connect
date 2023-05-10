4fbfb5b09dcebb8bfcae4d0e573777e9b082736b
saklani <50768838+saklani@users.noreply.github.com>
2023-05-10 17:17:40 +0530

    remove balance from exposed store

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,8 @@
 // Reexport your entry components here
 import ConnectButton from "./ConnectButton.svelte";
-import { account, balance, network } from "./store.js";
+import { account, network } from "./store.js";
 import { getDefaultConnectors } from "./wallet/connectors.js";
 
 export default ConnectButton;
 
-export { account, network, balance, getDefaultConnectors };
-
+export { account, network, getDefaultConnectors };
