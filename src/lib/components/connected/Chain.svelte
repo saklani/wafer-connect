7bd05578a67d1904660675d878b8ea5d42dae4ee
saklani <50768838+saklani@users.noreply.github.com>
2023-05-18 15:26:06 +0530

    feat: chain now has down arrow

--- src/lib/components/connected/Chain.svelte
+++ src/lib/components/connected/Chain.svelte
@@ -26,10 +26,37 @@
   disabled={isSwitchingNetwork || $chains.length === 1}
   style="border-radius: 999px;"
 >
-  {isSwitchingNetwork ? "Switching•••" : $network.chain.name}
+  {#if isSwitchingNetwork}
+    <p>"Switching•••"</p>
+  {:else if $chains.length === 1}
+    <p>{$network.chain.name}</p>
+  {:else}
+    <p>{$network.chain.name}</p>
+    <svg
+      style="width: 12px; height: 12px;"
+      width="26"
+      height="25"
+      viewBox="0 0 26 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M24.6289 11.8813L13.3491 23.1612"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="square"
+      />
+      <path
+        d="M2 11.8799L13.2798 23.1597"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="square"
+      />
+    </svg>
+  {/if}
 </button>
 
-<Dialog bind:showDialog --max-width="200px">
+<Dialog bind:showDialog --max-width="200px" --dialog-blur="blur(0px)">
   <h3 slot="title">Chains</h3>
   {#each $chains as chain}
     {#if chain.id !== $network.chain.id}
@@ -43,7 +70,6 @@
 <style>
   :root {
     --chain-color: black;
-    --height: 32px;
     --border-radius: 6px;
   }
   button {
@@ -52,7 +78,8 @@
     height: var(--height);
     border: solid 1px var(--chain-color);
     border-radius: var(--border-radius);
-    padding-block: 6px;
+    padding-block: 8px;
+    padding-inline: 12px;
   }
   button:hover {
     background-color: rgb(192, 192, 192, 0.1);
