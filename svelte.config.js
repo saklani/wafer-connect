8b92293e5c3d6b9713288e6c5597709e0465c520
saklani <50768838+saklani@users.noreply.github.com>
2023-05-29 01:47:22 +0530

    docs: improve getting started

--- svelte.config.js
+++ svelte.config.js
@@ -2,18 +2,13 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from "mdsvex";
 
-function highlight(code, lang) {
-  return `<pre><code class="language-${lang}">${code}</code></pre>`;
-}
-
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({}),
   },
-  extensions: [".svelte", ".svx"],
-  preprocess: [mdsvex(), vitePreprocess()],
+  extensions: [".svelte", ".svx"] ,
+  preprocess: [mdsvex({  extensions: [".svelte", ".svx"] }), vitePreprocess()],
 };
 
 export default config;
