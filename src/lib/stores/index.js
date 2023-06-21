01d87a123287bdaeeb5c20cb2feddc19dddc182b
saklani <50768838+saklani@users.noreply.github.com>
2023-06-22 00:02:42 +0530

    fix: imports and docs for stores:

--- src/lib/stores/index.js
+++ src/lib/stores/index.js
@@ -1,6 +1,4 @@
 import { account } from "./account.js";
-import { block } from "./block.js";
-import { balance } from "./balance.js";
 import { network } from "./network.js";
 
-export { account, balance, block, network };
+export { account, network };
